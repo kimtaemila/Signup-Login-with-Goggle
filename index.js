08ca4fe5d48d1511da7e84bb3d2e1a1ade69354e
kimtaemila <rudmilaarafin@gmail.com>
2023-06-02 11:09:42 -0400

    It works!

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('loginButton').addEventListener('click', function () {
+    // Redirect to the next page
+    window.location.href = 'public/login/login.html';
+});
+
+document.getElementById('signupButton').addEventListener('click', function () {
+    // Redirect to the next page
+    window.location.href = 'public/signup/signup.html';
+});
